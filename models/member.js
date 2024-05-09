c631aba8b6f062c0b10c12cb4349123308ff551a
amrmohamed <amoree.mb@gmail.com>
2024-05-09 16:26:31 +0300

    Create member schema

---
+++ models/member.js
@@ -0,0 +1,17 @@
+const { types } = require("@babel/core");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const memberSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  username: { type: String, required: true },
+  passsword: { type: String, required: true },
+  admin: { type: Boolean, default: false },
+  messages: {
+    type: Schema.Types.ObjectId,
+    ref: "Messages",
+  },
+});
+
+module.exports = mongoose.model("Member", memberSchema);
