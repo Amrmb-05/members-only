c6b5afd1c7717ca29eb68c4ab83992dd3fb43a7d
amrmohamed <amoree.mb@gmail.com>
2024-05-10 15:32:05 +0300

    Write contoller function for signup post requests

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -6,4 +6,54 @@ exports.createMember_get = (req, res, next) => {
   res.render("sign-up");
 };
 
-exports.createMember_post = asyncHandler(async (req, res, next) => {});
+exports.createMember_post = [
+  body("firstname")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("First name can not be empty ")
+    .isAlpha()
+    .withMessage("Name should only contain alphabet characters")
+    .escape(),
+  body("lastname")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Last name can not be empty ")
+    .isAlpha()
+    .withMessage("Name should only contain alphabet characters")
+    .escape(),
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("User name can not be empty ")
+    .isEmail()
+    .withMessage("Please enter a valid email address")
+    .escape(),
+  body("password")
+    .trim()
+    .isLength({ min: 7 })
+    .withMessage("Password must contain atleast 7 characters.")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create member object with escaped and trimmed data
+
+    const member = new Member({
+      first_name: req.body.firstname,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      passsword: req.body.passsword,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("/sign-up", {
+        member: member,
+        errors: errors.array(),
+      });
+    } else {
+      await member.save();
+      res.redirect("/sign-in");
+    }
+  }),
+];
