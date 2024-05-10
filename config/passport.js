1d47174b9a5310a9a70227f406f62f7bfaeec50e
amrmohamed <amoree.mb@gmail.com>
2024-05-10 17:05:40 +0300

    Set up passport local strategy

---
+++ config/passport.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const Member = require("../models/member");
+const LocalStrategy = require("passport-local").Strategy;
+const asyncHandler = require("express-async-handler");
+
+passport.use(
+  asyncHandler(async (username, password, done) => {
+    const member = await Member.findOne({ username: username });
+    const passwordMatch = bcrypt.compare(password, member.password);
+    if (!member || !passwordMatch) {
+      return done(null, false, { message: "Incorrect username or password." });
+    }
+    return done(null, member);
+  }),
+);
+
+passport.serializeUser((member, done) => {
+  done(null, member.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const member = await Member.findById(id);
+    done(null, member);
+  } catch (err) {
+    done(err);
+  }
+});
