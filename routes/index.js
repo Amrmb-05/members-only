c89e3e92d0f2e58ef9eb8e60bcbd6589b420e264
amrmohamed <amoree.mb@gmail.com>
2024-05-11 00:56:57 +0300

    Make the / route display the current user if someone is logged in

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const signUpController = require("../controllers/signUpController");
 const loginController = require("../controllers/loginController");
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", member: req.user });
 });
 
 // Sign-up page routes
