9ea4bb051a78c205877e90a3b9e185180577aed5
amrmohamed <amoree.mb@gmail.com>
2024-05-10 23:31:57 +0300

    Add login get route

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,5 +4,5 @@ const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 
 exports.login_get = (req, res, next) => {
-  res.render("login.ejs");
+  res.render("login");
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const signUpController = require("../controllers/signUpController");
+const loginController = require("../controllers/loginController");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -9,4 +10,7 @@ router.get("/", function (req, res, next) {
 // Sign-up page routes
 router.get("/sign-up", signUpController.createMember_get);
 router.post("/sign-up", signUpController.createMember_post);
+
+// Login page routes
+router.get("/login", loginController.login_get);
 module.exports = router;
