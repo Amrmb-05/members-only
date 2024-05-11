e8cc5ee18af579cecc7d90dbb4c8b2aedbb271e2
amrmohamed <amoree.mb@gmail.com>
2024-05-11 16:18:38 +0300

    Add unique property to username field

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 const userSchema = new Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   member: { type: Boolean, default: false },
   admin: { type: Boolean, default: false },
