19b6d5f093c0850bfb624956acef80bd024507b2
amrmohamed <amoree.mb@gmail.com>
2024-05-12 21:51:31 +0300

    Add space between first and last name in the fullname virtual field

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
 userSchema.virtual("name").get(function () {
   let fullname = "";
   if (this.first_name && this.last_name) {
-    fullname = this.first_name + this.last_name;
+    fullname = `${this.first_name} ${this.last_name}`;
   }
   return fullname;
 });
