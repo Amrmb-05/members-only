ef5f6c4a2f71717a6122b73490da0fd53b495e30
amrmohamed <amoree.mb@gmail.com>
2024-05-12 23:12:43 +0300

    Write becomeMember_post function to be used in the member posr route

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -5,3 +5,23 @@ const { body, validationResult } = require("express-validator");
 exports.becomeMember_get = asyncHandler(async (req, res, next) => {
   res.render("member-form", { user: req.user });
 });
+
+exports.becomeMember_post = [
+  body("member-passcode").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    // Check if the user entered the correct passcode
+    if (req.body.member_passcode === process.env.MEMBER_PASSCODE) {
+      const member = await User.findOneAndUpdate(
+        { username: req.user.username },
+        { member: true },
+        { new: true },
+      );
+      res.redirect("/");
+    } else {
+      res.render("member-form", {
+        user: req.user,
+      });
+    }
+  }),
+];
