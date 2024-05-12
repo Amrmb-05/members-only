71e0be3f4143895dec504523d2b14f9734b2d4b1
amrmohamed <amoree.mb@gmail.com>
2024-05-12 23:25:04 +0300

    Write becomeAdmin function to be used for the admin post route

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -29,3 +29,23 @@ exports.becomeMember_post = [
 exports.becomeAdmin_get = asyncHandler(async (req, res, next) => {
   res.render("admin-form", { user: req.user });
 });
+
+exports.becomeAdmin_post = [
+  body("admin_passcode").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    // Check if the user entered the correct passcode
+    if (req.body.admin_passcode === process.env.ADMIN_PASSCODE) {
+      await User.findOneAndUpdate(
+        { username: req.user.username },
+        { admin: true, member: true },
+        { new: true },
+      );
+      res.redirect("/");
+    } else {
+      res.render("admin-form", {
+        user: req.user,
+      });
+    }
+  }),
+];
