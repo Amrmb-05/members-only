0de6e6c55693810600d03f9ac1128e7616f10281
amrmohamed <amoree.mb@gmail.com>
2024-05-16 12:10:35 +0300

    Display error messages if user enters invalid credentials

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,10 +6,29 @@ const passport = require("passport");
 const { body, validationResult } = require("express-validator");
 
 exports.login_get = (req, res, next) => {
-  res.render("login");
+  res.render("login", { error: undefined });
 };
 
 exports.login_post = passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/login",
 });
+
+exports.login_post = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      // Password is incorrect
+
+      return res.render("login", { error: info.message });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
+};
